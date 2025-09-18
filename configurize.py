325fade6fedaed62b5d6cac2ca37a7961ab4cebe
Phuc X. Doan <phucxdoan@gmail.com>
2025-09-17 22:01:00 -0400

    Cleaned up some interrupt stuff.

--- configurize.py
+++ configurize.py
@@ -215,42 +215,6 @@ def configurize(Meta, parameterization):
 
 
 
-        # Check to make sure the interrupts
-        # to be used by the target exists.
-
-        for interrupt, niceness in target.interrupts:
-
-            if interrupt not in parameterization('INTERRUPTS'):
-
-                raise ValueError(
-                    f'For target {repr(target.name)}, '
-                    f'no such interrupt {repr(interrupt)} '
-                    f'exists on {repr(target.mcu)}; '
-                    f'did you mean any of the following? : '
-                    f'{difflib.get_close_matches(
-                        str(interrupt),
-                        map(str, parameterization('INTERRUPTS').keys()),
-                        n      = 5,
-                        cutoff = 0
-                    )}'
-                )
-
-
-
-        # @/`Defining Interrupt Handlers`.
-
-        for interrupt in sorted(dict.fromkeys((
-            *target.interrupts_that_must_be_defined,
-            *(name for name, niceness in target.interrupts)
-        ))):
-
-            Meta.define(
-                f'INTERRUPT_{interrupt}',
-                f'extern void INTERRUPT_{interrupt}(void)'
-            )
-
-
-
         # Configure the interrupt priorities.
 
         for interrupt, niceness in target.interrupts:
@@ -713,47 +677,3 @@ def configurize(Meta, parameterization):
     ################################################################################
 
     Meta.line()
-
-
-
-################################################################################
-
-
-
-# @/`Defining Interrupt Handlers`:
-#
-# Most other applications use weak-symbols as a way to have
-# the user be able to declare their interrupt routines, but
-# also have a default routine to fallback on if the user didn't
-# do so.
-#
-# However, this leaves for a potential bug where the user makes
-# a typo and ends up declaring a useless function that the
-# linker then ignores and the intended ISR will still be
-# referring to the default interrupt handler.
-#
-# e.g:
-# >
-# >    extern void
-# >    INTERRUPT_I2C1_EB   <- Typo!
-# >    {
-# >        ...
-# >    }
-# >
-#
-# To address this, macros will be made specifically for only
-# expected ISRs to be defined by the target. If the macro
-# doesn't exist, then this ends up being a compilation error,
-# but otherwise the macro expands to the expected function
-# prototype.
-#
-# e.g:
-# >
-# >    INTERRUPT_I2C1_EB   <- Compile error!
-# >    {
-# >        ...
-# >    }
-# >
-#
-# Not only that, we can also prevent the user from trying to
-# define an ISR that's not expected to be used by the target.
--- init.py
+++ init.py
@@ -9,6 +9,32 @@ def init(Meta, target):
 
 
 
+    ################################################################################
+
+
+
+    # Check to make sure the interrupts
+    # to be used by the target exists.
+
+    for interrupt, niceness in target.interrupts:
+
+        if interrupt not in MCUS[target.mcu]['INTERRUPTS'].value:
+
+            raise ValueError(
+                f'For target {repr(target.name)}, '
+                f'no such interrupt {repr(interrupt)} '
+                f'exists on {repr(target.mcu)}; '
+                f'did you mean any of the following? : '
+                f'{difflib.get_close_matches(
+                    str(interrupt),
+                    map(str, MCUS[target.mcu]['INTERRUPTS'].value.keys()),
+                    n      = 5,
+                    cutoff = 0
+                )}'
+            )
+
+
+
     # For interrupts used by the target that
     # are in NVIC, we create an enumeration so
     # that the user can only enable those specific
@@ -27,55 +53,61 @@ def init(Meta, target):
 
 
 
-    # TODO.
+    # We forward-declare the prototype of
+    # the interrupt service routines.
 
     for interrupt in sorted(dict.fromkeys((
-        'Reset',
         *target.interrupts_that_must_be_defined,
         *(name for name, niceness in target.interrupts)
     ))):
 
         Meta.line(f'''
             extern void INTERRUPT_{interrupt}(void);
-            extern nullptr_t LINK_stack_addr[];
         ''')
 
 
 
-    # TODO.
+    # We create the interrupt vector table.
 
     with Meta.enter('''
-        void (* const INTERRUPT_VECTOR_TABLE[])(void) __attribute__((used, section(".vector_table"))) =
+        void (* const INTERRUPT_VECTOR_TABLE[])(void)__attribute__((used, section(".INTERRUPT_VECTOR_TABLE"))) =
     ''', '{', '};', indented = True):
 
+
+
+        # The first entry in the Arm IVT is actually the
+        # initial value of $SP after a reset of the CPU.
+
         rows = [
-            (f'(void*) &LINK_stack_addr', -16, 'Initial Stack Address'),
+            (f'(void*) INITIAL_STACK_ADDRESS', -16, 'Initial Stack Address'),
         ]
 
-        for interrupt_i, interrupt_name in enumerate(MCUS[target.mcu]['INTERRUPTS'].value):
 
-            interrupt_number = interrupt_i - 15
+
+        # Fill in the addresses of the interrupt routines
+        # for the remainder of the interrupt vector table.
+
+        for interrupt_i, interrupt in enumerate(MCUS[target.mcu]['INTERRUPTS'].value):
 
             word = None
 
-            if interrupt_name is None:
+            if interrupt is None:
 
                 # No interrupt handler here.
                 word = f'INTERRUPT_Default'
 
-            elif target.use_freertos and interrupt_name in MCUS[target.mcu].freertos_interrupts:
+            elif target.use_freertos and interrupt in MCUS[target.mcu].freertos_interrupts:
 
                 # This interrupt will be supplied by FreeRTOS.
-                word = f'{MCUS[target.mcu].freertos_interrupts[interrupt_name]}'
+                word = f'{MCUS[target.mcu].freertos_interrupts[interrupt]}'
 
-            elif interrupt_name in (
-                'Reset',
+            elif interrupt in (
                 *target.interrupts_that_must_be_defined,
                 *(name for name, niceness in target.interrupts)
             ):
 
-                # This interrupt must be defined somewhere.
-                word = f'INTERRUPT_{interrupt_name}'
+                # This interrupt will be defined by the user.
+                word = f'INTERRUPT_{interrupt}'
 
             else:
 
@@ -84,23 +116,41 @@ def init(Meta, target):
 
             rows += [(
                 word,
-                interrupt_number,
-                'Reserved' if interrupt_name is None else interrupt_name,
+                interrupt_i - 15,
+                'Reserved' if interrupt is None else interrupt,
             )]
 
 
 
-        # Output nice looking table.
+        # Output a nice looking table.
 
-        for word, interrupt_number, name in justify(
+        for word, number, name in justify(
             (
-                ('<' , word            ),
-                ('>' , interrupt_number),
-                (None, name            ),
+                ('<' , word  ),
+                ('>' , number),
+                (None, name  ),
             )
-            for word, interrupt_number, name in rows
+            for word, number, name in rows
         ):
-            Meta.line(f'{word}, // [{interrupt_number}] {name}')
+            Meta.line(f'{word}, // [{number}] {name}')
+
+
+
+    # @/`Defining Interrupt Handlers`.
+
+    for interrupt in sorted(dict.fromkeys((
+        *target.interrupts_that_must_be_defined,
+        *(name for name, niceness in target.interrupts)
+    ))):
+
+        Meta.define(
+            f'INTERRUPT_{interrupt}',
+            f'extern void INTERRUPT_{interrupt}(void)'
+        )
+
+
+
+    ################################################################################
 
 
 
@@ -120,3 +170,47 @@ def init(Meta, target):
     '''):
 
         configurize(Meta, parameterization)
+
+
+
+################################################################################
+
+
+
+# @/`Defining Interrupt Handlers`:
+#
+# Most other applications use weak-symbols as a way to have
+# the user be able to declare their interrupt routines, but
+# also have a default routine to fallback on if the user didn't
+# do so.
+#
+# However, this leaves for a potential bug where the user makes
+# a typo and ends up declaring a useless function that the
+# linker then ignores and the intended ISR will still be
+# referring to the default interrupt handler.
+#
+# e.g:
+# >
+# >    extern void
+# >    INTERRUPT_I2C1_EB   <- Typo!
+# >    {
+# >        ...
+# >    }
+# >
+#
+# To address this, macros will be made specifically for only
+# expected ISRs to be defined by the target. If the macro
+# doesn't exist, then this ends up being a compilation error,
+# but otherwise the macro expands to the expected function
+# prototype.
+#
+# e.g:
+# >
+# >    INTERRUPT_I2C1_EB   <- Compile error!
+# >    {
+# >        ...
+# >    }
+# >
+#
+# Not only that, we can also prevent the user from trying to
+# define an ISR that's not expected to be used by the target.
