7337b5d333e5fc84c57a842cbb1fd4ce864ddf8a
Phuc Doan <phucxdoan@gmail.com>
2026-01-17 17:01:00 -0500

    Removed need for `STPY_IMPLEMENTATION`.

--- init.py
+++ init.py
@@ -40,182 +40,178 @@ def init(
 
 
 
-    with Meta.enter('#if STPY_IMPLEMENTATION'): # TODO This is here for the niche reasons...
+    # For interrupts used by the target that
+    # are in NVIC, we create an enumeration so
+    # that the user can only enable those specific
+    # interrupts. Note that some interrupts, like
+    # SysTick, are not a part of NVIC.
+
+    Meta.enums(
+        'NVICInterrupt',
+        'u32',
+        (
+            (interrupt.name, f'{interrupt.name}_IRQn')
+            for interrupt in parameterization.interrupts
+            if interrupt.number >= 0
+        )
+    )
 
 
 
-        # For interrupts used by the target that
-        # are in NVIC, we create an enumeration so
-        # that the user can only enable those specific
-        # interrupts. Note that some interrupts, like
-        # SysTick, are not a part of NVIC.
+    # We forward-declare the prototype of
+    # the interrupt service routines.
 
-        Meta.enums(
-            'NVICInterrupt',
-            'u32',
+    for name in sorted((
+        'INTERRUPT_Default',
+        *(
+            interrupt.symbol
+            for interrupt in parameterization.interrupts
+        )
+    )):
+
+        Meta.line(f'''
+            extern void {name}(void);
+        ''')
+
+
+
+    # We create the interrupt vector table.
+
+    with Meta.enter('''
+        void (* const INTERRUPT_VECTOR_TABLE[])(void) __attribute__((used, section(".INTERRUPT_VECTOR_TABLE"))) =
+    ''', '{', '};', indented = True):
+
+
+
+        # The first entry in the Arm IVT is actually the
+        # initial value of $SP after a reset of the CPU.
+
+        rows = [
+            (f'(void*) INITIAL_STACK_ADDRESS', -16, 'Initial Stack Address'),
+        ]
+
+
+
+        # For interrupts that the target will be using,
+        # we put those interrupts into their corresponding
+        # slots in the interrupt vector table; everything
+        # else will be to the default interrupt handler.
+
+        for table_interrupt_i, table_interrupt in enumerate(MCUS[parameterization.mcu]['INTERRUPTS'].value):
+
+            rows += [(
+                next(
+                    (
+                        target_interrupt.symbol
+                        for target_interrupt in parameterization.interrupts
+                        if target_interrupt.name == table_interrupt
+                    ),
+                    f'INTERRUPT_Default'
+                ),
+                table_interrupt_i - 15,
+                'Reserved' if table_interrupt is None else table_interrupt,
+            )]
+
+
+
+        # Output a nice looking table.
+
+        for word, number, name in pxd.justify(
             (
-                (interrupt.name, f'{interrupt.name}_IRQn')
-                for interrupt in parameterization.interrupts
-                if interrupt.number >= 0
+                ('<' , word  ),
+                ('>' , number),
+                (None, name  ),
             )
+            for word, number, name in rows
+        ):
+            Meta.line(f'{word}, // [{number}] {name}')
+
+
+
+    # @/`Defining Interrupt Handlers`.
+
+    for interrupt in sorted((
+        'Default',
+        *(
+            interrupt.name
+            for interrupt in parameterization.interrupts
+        )
+    )):
+
+        Meta.define(
+            f'INTERRUPT_{interrupt}',
+            f'extern void INTERRUPT_{interrupt}(void)'
         )
 
 
 
-        # We forward-declare the prototype of
-        # the interrupt service routines.
+    # Forward-declare the interrupt callback for GPIOs with EXTI.
 
-        for name in sorted((
-            'INTERRUPT_Default',
-            *(
-                interrupt.symbol
-                for interrupt in parameterization.interrupts
-            )
-        )):
+    for gpio in parameterization.gpios:
 
-            Meta.line(f'''
-                extern void {name}(void);
-            ''')
+        if gpio.interrupt is None:
+            continue
+
+        Meta.line(f'''
+            static void
+            INTERRUPT_EXTIx_{gpio.name}(void);
+        ''')
 
 
 
-        # We create the interrupt vector table.
+    # When the EXTI interrupt is called,
+    # we invoke the callback of the GPIOs associated with it
+    # if their interrupt flag is pending.
 
-        with Meta.enter('''
-            void (* const INTERRUPT_VECTOR_TABLE[])(void) __attribute__((used, section(".INTERRUPT_VECTOR_TABLE"))) =
-        ''', '{', '};', indented = True):
+    for exti_number, gpios in pxd.coalesce(
+        (gpio.number, gpio)
+        for gpio in parameterization.gpios
+        if gpio.interrupt is not None
+    ):
 
+        with Meta.enter(f'INTERRUPT_EXTI{exti_number}'):
 
+            for gpio in gpios:
 
-            # The first entry in the Arm IVT is actually the
-            # initial value of $SP after a reset of the CPU.
+                peripheral, register, field = ( # TODO Dumbest way to do this.
+                    MCUS
+                        [parameterization.mcu]
+                        [f'EXTI{exti_number}_PENDING_{gpio.interrupt}_INTERRUPT']
+                ).location
 
-            rows = [
-                (f'(void*) INITIAL_STACK_ADDRESS', -16, 'Initial Stack Address'),
-            ]
-
-
-
-            # For interrupts that the target will be using,
-            # we put those interrupts into their corresponding
-            # slots in the interrupt vector table; everything
-            # else will be to the default interrupt handler.
-
-            for table_interrupt_i, table_interrupt in enumerate(MCUS[parameterization.mcu]['INTERRUPTS'].value):
-
-                rows += [(
-                    next(
-                        (
-                            target_interrupt.symbol
-                            for target_interrupt in parameterization.interrupts
-                            if target_interrupt.name == table_interrupt
-                        ),
-                        f'INTERRUPT_Default'
-                    ),
-                    table_interrupt_i - 15,
-                    'Reserved' if table_interrupt is None else table_interrupt,
-                )]
+                Meta.line(f'''
+                    if (CMSIS_GET({peripheral}, {register}, {field}))
+                    {{
+                        CMSIS_SET({peripheral}, {register}, {field}, true);
+                        INTERRUPT_EXTIx_{gpio.name}();
+                    }}
+                ''')
 
 
 
-            # Output a nice looking table.
+    # Make the EXTI interrupt callback macro to ensure
+    # no extraneous declarations are made.
 
-            for word, number, name in pxd.justify(
-                (
-                    ('<' , word  ),
-                    ('>' , number),
-                    (None, name  ),
-                )
-                for word, number, name in rows
-            ):
-                Meta.line(f'{word}, // [{number}] {name}')
+    for gpio in parameterization.gpios:
 
+        if gpio.interrupt is None:
+            continue
 
-
-        # @/`Defining Interrupt Handlers`.
-
-        for interrupt in sorted((
-            'Default',
-            *(
-                interrupt.name
-                for interrupt in parameterization.interrupts
-            )
-        )):
-
-            Meta.define(
-                f'INTERRUPT_{interrupt}',
-                f'extern void INTERRUPT_{interrupt}(void)'
-            )
+        Meta.define(
+            f'INTERRUPT_EXTIx_{gpio.name}',
+            f'static void INTERRUPT_EXTIx_{gpio.name}(void)'
+        )
 
 
 
-        # Forward-declare the interrupt callback for GPIOs with EXTI.
+    # Configurization.
 
-        for gpio in parameterization.gpios:
+    with Meta.enter('''
+        extern void
+        STPY_init(void)
+    '''):
 
-            if gpio.interrupt is None:
-                continue
-
-            Meta.line(f'''
-                static void
-                INTERRUPT_EXTIx_{gpio.name}(void);
-            ''')
-
-
-
-        # When the EXTI interrupt is called,
-        # we invoke the callback of the GPIOs associated with it
-        # if their interrupt flag is pending.
-
-        for exti_number, gpios in pxd.coalesce(
-            (gpio.number, gpio)
-            for gpio in parameterization.gpios
-            if gpio.interrupt is not None
-        ):
-
-            with Meta.enter(f'INTERRUPT_EXTI{exti_number}'):
-
-                for gpio in gpios:
-
-                    peripheral, register, field = ( # TODO Dumbest way to do this.
-                        MCUS
-                            [parameterization.mcu]
-                            [f'EXTI{exti_number}_PENDING_{gpio.interrupt}_INTERRUPT']
-                    ).location
-
-                    Meta.line(f'''
-                        if (CMSIS_GET({peripheral}, {register}, {field}))
-                        {{
-                            CMSIS_SET({peripheral}, {register}, {field}, true);
-                            INTERRUPT_EXTIx_{gpio.name}();
-                        }}
-                    ''')
-
-
-
-        # Make the EXTI interrupt callback macro to ensure
-        # no extraneous declarations are made.
-
-        for gpio in parameterization.gpios:
-
-            if gpio.interrupt is None:
-                continue
-
-            Meta.define(
-                f'INTERRUPT_EXTIx_{gpio.name}',
-                f'static void INTERRUPT_EXTIx_{gpio.name}(void)'
-            )
-
-
-
-        # Configurization.
-
-        with Meta.enter('''
-            extern void
-            STPY_init(void)
-        '''):
-
-            configurize(Meta, parameterization)
+        configurize(Meta, parameterization)
 
 
 
